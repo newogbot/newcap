60e85bc3072cd35e5507601d1ac056fbd61ba926
𖤍ᴛʜᴇ / ᴄᴀᴘᴛᴀɪɴ𖤍 <167273277+itzAsuraa@users.noreply.github.com>
2024-08-09 13:22:00 +0530

    Delete AnonXMusic/utils/itzAsuraa.py

--- AnonXMusic/utils/itzAsuraa.py
+++
@@ -1,7 +0,0 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram import Client, filters, enums 
-
-class BUTTONS(object):
-    MBUTTON = [[InlineKeyboardButton("<", callback_data=f"settings_back_helper"), 
-    InlineKeyboardButton(">", callback_data=f"managebot123 settings_back_helper"),
-    ]]
